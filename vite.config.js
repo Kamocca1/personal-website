0c6c355aa2a6c32088a898b6c12e3394da9646ec
Kamocca <86765533+Kamocca1@users.noreply.github.com>
2026-05-15 19:34:01 +0200

    Install shadcn UI

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import path from "node:path";
+
 import { defineConfig } from "vite";
 
 export default defineConfig({
