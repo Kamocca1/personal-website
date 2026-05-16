322c7ddd5525cb5bb1d7e6d96ba7785838ba6f22
Kamocca <86765533+Kamocca1@users.noreply.github.com>
2026-05-16 16:58:12 +0200

    Changes: Project initialized and Tailwind CSS and Shadcn UI integrated.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import { Button } from "@/components/ui/button.jsx";
+
 const App = () => {
-    return <div className="text-blue-600 font-bold text-4xl">App</div>;
+    return (
+        <div className="text-blue-600 font-bold text-4xl">
+            <Button size="icon">Click Me</Button>
+        </div>
+    );
 };
 
 export default App;
